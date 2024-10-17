5976f3b954698f749e6ffe889f3878b642c23900
Linus <linusoberg97@hotmail.com>
2024-10-17 17:52:05 +0200

    Add project files.

---
+++ MovieVault/Data/Movie.cs
@@ -0,0 +1,16 @@
+﻿namespace MovieVault.Data
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Runtime { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Actors { get; set; }
+        public string Plot { get; set; }
+        public string imdbRating { get; set; }
+        public string Poster { get; set; }
+    }
+}
