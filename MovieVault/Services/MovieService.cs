503c3dd16ea968e22eb6c17fe81f3640104d7cac
Linus <linusoberg97@hotmail.com>
2024-10-21 11:46:31 +0200

    Finished MovieService at least for now

--- MovieVault/Services/MovieService.cs
+++ MovieVault/Services/MovieService.cs
@@ -41,6 +41,23 @@ public class MovieService(ApplicationDbContext _context, HttpClient _httpClient,
         return viewData;
     }
 
+    public async Task DeleteMovieAsync(int id)
+    {
+        var data = await _context.Movies.FirstOrDefaultAsync(x => x.Id == id);
+        if(data != null)
+        {
+            _context.Movies.Remove(data);
+            _context.SaveChanges();
+        }
+    }
+
+    public async Task SaveMovieAsync(MovieDescriptionVM data)
+    {
+        var movie = _mapper.Map<MovieDescriptionVM>(data);
+        _context.Add(movie);
+        await _context.SaveChangesAsync();
+    }
+
     public async Task<bool> CheckIfMovieExists(string name)
     {
         var lowercaseName = name.ToLower();
