cb62a97759b87b9fc0d48cd3fb641d3092389d0c
Linus <linusoberg97@hotmail.com>
2024-10-28 11:17:29 +0100

    Made property UserId of object Movie to a FK of IdentityUser Id.

--- MovieVault/Data/ApplicationDbContext.cs
+++ MovieVault/Data/ApplicationDbContext.cs
@@ -1,15 +1,27 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace MovieVault.Data
-{
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+namespace MovieVault.Data;
 
-        public DbSet<Movie> Movies { get; set; } 
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Movie> Movies { get; set; }
+
+    // Makes property UserId of object Movie to a FK of IdentityUser Id.
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Movie>()
+            .HasOne<IdentityUser>()                
+            .WithMany()                            
+            .HasForeignKey(m => m.UserId)          
+            .OnDelete(DeleteBehavior.Cascade);     
     }
 }
---
+++ MovieVault/Data/Migrations/20241028100629_AddUserForeignKeyToMovie.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieVault.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserForeignKeyToMovie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Movies",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_UserId",
+                table: "Movies",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_AspNetUsers_UserId",
+                table: "Movies",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_AspNetUsers_UserId",
+                table: "Movies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movies_UserId",
+                table: "Movies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
